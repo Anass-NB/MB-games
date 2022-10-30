c2ef10e338b030a945057c3875745968ad98a8e5
Anass-NB <anassnabil067@gmail.com>
2022-10-30 15:21:10 +0100

    Configure layouts files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends("layouts.app")
+@section('css')
+
+@section('title')
+    Home
+@stop
+@endsection
+
+@section('content')
+{{-- <!-- row -->
+<div class="row">
+    <div class="col-md-12 mb-30">
+        <div class="card card-statistics h-100">
+            <div class="card-body">
+                <p>Page content goes here<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- row closed --> --}}
+@endsection
+
+@section('js')
+
+@endsection
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,22 @@
+<!-- Title -->
+<title>@yield("title")</title>
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico') }}" type="image/x-icon" />
+
+<!-- Font -->
+<link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Poppins:200,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900">
+@yield('css')
+<!--- Style css -->
+<link href="{{ URL::asset('assets/css/style.css') }}" rel="stylesheet">
+
+<!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+<!--- Style css -->
+@if (App::getLocale() == 'en')
+    <link href="{{ URL::asset('assets/css/ltr.css') }}" rel="stylesheet">
+@else
+    <link href="{{ URL::asset('assets/css/rtl.css') }}" rel="stylesheet">
+@endif
