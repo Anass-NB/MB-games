4ba612b3cbb04c955285e0af8eedc1c193ed7aac
unknown <sallah.mouradi19@gmail.com>
2022-10-31 15:28:19 +0100

    updates done by mouradi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,25 @@
-@extends("layouts.app")
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('css')
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-  <link rel="stylesheet" href="{{ URL::asset("assets/home.css") }}">
-  <!-- CSS only -->
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+<link rel="stylesheet" href="{{ URL::asset("assets/home.css") }}">
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        
 
-@endsection
 
-@section('title')
-  Home
-{{-- @stop --}}
-@endsection
 
-@section('content')
+
+
 <div class="landing-page">
   <header class="main-header">
     <div class="logo">
@@ -43,4 +50,6 @@
   </div>
 </div>
 
-@endsection
+
+</body>
+</html>
