8295b25352b4d0e08f65669f675d0fff0b0d8c73
Anass-NB <anassnabil067@gmail.com>
2022-11-09 13:21:05 +0100

    Wesh A3emi voila rah tgad chwiya Front

---
+++ resources/views/allgames.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ALL GAMES</title>
+  <link rel="icon" type="image/x-icon" href="{{ asset("images/favicon.png") }}">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
+
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/allgames.css') }}">
+</head>
+<body class="font-sans antialiased allgameshtml">
+  <div class="landing">
+    <div class="overlay"></div>
+    <div class="header">
+      <div class="container2 d-flex justify-content-between">
+        <div class="logo">
+          <img src="{{ asset("images/lloooogo.png") }}" alt="logo"><a href="{{ route("home_front") }}" class="logo"><span>W</span>eb  <span>G</span>ames</a>
+        </div>
+        <ul class="main-nav bg-sss">
+          <li><a href="#games">Games</a></li>
+          <li><a href="#categories">Categories</a></li>
+          <li><a href="#popular">Popular</a></li>
+          <li><a class="active" href="{{ route("login") }}">Login</a></li>
+      </div>   
+    </div>
+    {{-- Start Games --}}
+    <div class="games">
+      <div class="container">
+        <div class="row category">
+          <h1 class="nameofcategory">Action Games</h1>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-02.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-02.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-02.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-02.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-02.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-02.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+        </div>
+        <div class="row category">
+          <h1 class="nameofcategory">Car Games</h1>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-06.png") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-06.png") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-06.png") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-06.png") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-06.png") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-06.png") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+        </div>
+        <div class="row category">
+          <h1 class="nameofcategory">Ninja Games</h1>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-04.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-04.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-04.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-04.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-04.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-04.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+        </div>
+        <div class="row category">
+          <h1 class="nameofcategory">Girls Games</h1>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-08.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-08.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-08.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-08.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-08.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+          <a href="{{ url("/allgames") }}" class="col text-center">
+            <div>
+              <img src="{{ asset("images/game-08.jpg") }}" alt="game">
+              <h5>Name of the game Here</h5>
+            </div>    
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+{{-- End Games --}}   
+  {{-- ENd landing page --}}
+
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,19 @@ use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name("home_front");
 Route::get('/home', function () {
     return view('home');
+});
+Route::get('/allgames', function () {
+    return view('allgames');
 });
 
 
 Route::get('/dashboard', function () {
     // $date_create_last_game = Game::where('id', '=',1    )->firstOrFail();
+
+    //hado mn be3d andirouhom f chi controller
     $date_create_last_game = Game::all()->last();
     $number_of_games = DB::table("games")->count();
     $number_of_users = DB::table("users")->count();
@@ -34,14 +39,25 @@ Route::get('/dashboard', function () {
     return view('dashboard',compact(["number_of_games","number_of_users","number_of_categories","date_create_last_game"]));
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/categories',[CategoryController::class,'index'])->middleware(['auth', 'verified'])->name('show_cat');
-Route::post("storingcategory",[CategoryController::class,'store'])->middleware(['auth', 'verified'])->name('category_store');
-Route::get("addgame",[GameController::class,"addnewgame"])->middleware(['auth', 'verified']);
-Route::post("/store",[GameController::class,"store"])->middleware(['auth', 'verified'])->name("store_game");
-Route::get("/deletingcategory/{id}",[CategoryController::class,'delete'])->middleware(['auth', 'verified'])->name('category_delete');
-Route::post("/updatecat",[CategoryController::class,'update'])->middleware(['auth', 'verified'])->name('category_update');
-Route::get("/fetchcat/{id}",[CategoryController::class,'fetch'])->middleware(['auth', 'verified'])->name('category_fetch');
-Route::get('/webgames',[GameController::class,'index'])->middleware(['auth', 'verified'])->name('show_games');
-Route::get('/retrievewebgames',[GameController::class,'retrievegames'])->middleware(['auth', 'verified'])->name('retrieve_games');
-Route::get("/editgame/{id}",[GameController::class,'edit'])->middleware(['auth', 'verified'])->name('edit_game');
+
+
+//Route Group Of Category Controller
+Route::controller(CategoryController::class)->group(function (){
+    Route::get('/categories','index')->middleware(['auth', 'verified'])->name('show_cat');
+    Route::post("storingcategory",'store')->middleware(['auth', 'verified'])->name('category_store');
+    Route::get("/deletingcategory/{id}",'delete')->middleware(['auth', 'verified'])->name('category_delete');
+    Route::post("/updatecat",'update')->middleware(['auth', 'verified'])->name('category_update');
+    Route::get("/fetchcat/{id}",'fetch')->middleware(['auth', 'verified'])->name('category_fetch');
+    
+});
+
+//Route Group Of Game Controller
+Route::controller(GameController::class)->group(function (){
+    Route::get('/webgames','index')->middleware(['auth', 'verified'])->name('show_games');
+    Route::get('/retrievewebgames','retrievegames')->middleware(['auth', 'verified'])->name('retrieve_games');
+    Route::get("/editgame/{id}",'edit')->middleware(['auth', 'verified'])->name('edit_game');
+    Route::get("addgame","addnewgame")->middleware(['auth', 'verified']);
+    Route::post("/store","store")->middleware(['auth', 'verified'])->name("store_game");
+
+});
 require __DIR__.'/auth.php';
